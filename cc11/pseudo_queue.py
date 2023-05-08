23103e5d5ff1c43038f94b29387924be0017af14
dinajar <deenajaradat2@gmail.com>
2023-05-08 20:17:19 +0300

    test

---
+++ cc11/pseudo_queue.py
@@ -0,0 +1,73 @@
+from node import Node
+from stack import Stack
+
+
+class Pseudo_queue:
+    def __init__(self):
+        self.top = None
+        self.stack2 = Stack()
+       
+    
+    def push(self, value):
+        node = Node(value)
+        node.next = self.top
+        self.top = node
+        return node.value  # return the value of the pushed node instead of the node object
+
+    def pop(self):
+        if self.top is None:
+            raise Exception("Empty stack")
+        
+        current = self.top
+        self.top = self.top.next
+        current.next = None
+        return current.value
+    
+    def is_empty(self):
+        """
+        Returns True if the stack is empty, False otherwise.
+        """
+        if self.top is None:   # Check if the stack is empty
+            
+            return True
+        return False
+    
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Empty stack")
+        return self.top.value
+
+    def enqueue(self, value ):
+        
+
+        if self.is_empty() :
+            self.push(value)
+
+        node = Node(value)
+            
+        while self.is_empty() == False:
+            self.stack2.push(self.pop())
+
+        self.push(node)
+
+        while self.stack2.top is not None:
+            self.push(self.stack2.pop())
+
+        return self
+    
+
+    
+
+if __name__ == "__main__":
+
+
+    S1 = Pseudo_queue()
+    S1.enqueue(10)
+    S1.enqueue(15)
+    S1.enqueue(20)
+   
+    print(S1.enqueue(5))  
+ 
+
+
