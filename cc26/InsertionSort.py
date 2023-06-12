26b73e18c8c20b5dd0092be1a3ff7a56aed8d30e
dinajar <deenajaradat2@gmail.com>
2023-06-13 01:48:40 +0300

    CC_26

--- cc26/InsertionSort.py
+++ cc26/InsertionSort.py
@@ -1,20 +1,26 @@
 def insert (sorted , value):
     i = 0
-    while value > len(sorted):
+    while value > sorted[i]:
         i+=1
+    # print(i)
     while i < len(sorted):
         temp = sorted[i]
         sorted[i] = value 
         value = temp
         i+=1
     sorted.append(value)
+    
 
 def InsertionSort(input):
     sorted = []
     sorted.append(input[0])
+    len_input = len(input)
+    # print(sorted)
 
-    for i in range (1, len(input)):
+    for i in range (1, len_input):
         insert(sorted , input[i])
+        print(sorted)
+        
     return sorted
 
 
